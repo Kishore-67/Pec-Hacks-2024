5afd4bbbd9c08f4cf66eaa7b62f5f261ef198d10
Kishore <126963491+Kishore-67@users.noreply.github.com>
2024-12-28 10:29:53 +0530

    routing changes

---
+++ Hackathon Live/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './Components/Dash';
+import './Styles/App.css';
+import Nav from './Components/Nav';
+import Home from './Components/Home';
+import Profile from './Components/Profile';
+import Signup from './Authentication/Signup';
+import Login from './Authentication/Login';
+import Scheduler from './Components/Scheduler';
+import Creator from './Components/Creators.js';
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/Post Scheduling" element={<Scheduler/>} />
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/analytics" element={<div>Analytics Page</div>} />
+          <Route path="/creators/*" element={<Creator/>} />
+          <Route path="/settings" element={<div>Settings Page</div>} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </Router>
+
+    
+
+  );
+}
+
+export default App;
